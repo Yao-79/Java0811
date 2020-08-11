6bef58869103d89d4b52172578ab5d12f236e3a7
MB-study <MB-study@192.168.1.88>
2020-08-11 14:01:12 +0800

    Java

--- src/main/java/com/pcschool/ocp/d01/ArrayTest1.java
+++ src/main/java/com/pcschool/ocp/d01/ArrayTest1.java
@@ -8,6 +8,24 @@ public class ArrayTest1 {
         System.out.println(scores[2]);
         System.out.println(scores[3]);
         System.out.println(scores.length);
+        int sum=0;
+        for(int i=0;i<scores.length;i++){
+            sum=sum+scores[i];
+        }
+        
+        System.out.printf("總合為: %d\n",sum);
+        
+        double avg= (double)sum/scores.length;
+        System.out.printf("平均為: %.2f\n",avg);
+        
+        double sum2=0;
+        for(int i=0;i<scores.length;i++){
+            sum2=sum+Math.pow(scores[i]-avg,2);
+        }
+        double sd=Math.sqrt((1.0/scores.length)*sum2);
+        System.out.printf("標準差: %.2f\n",sd);
+        double cv=sd/avg;
+        System.out.printf("變異係數: %.2f\n",cv);
         
     }
 }
