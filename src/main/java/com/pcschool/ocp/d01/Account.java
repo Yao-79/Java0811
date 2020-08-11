e549a2a43719828cadaf593ae24ab0f16a12ae8e
MB-study <MB-study@192.168.1.88>
2020-08-11 11:39:59 +0800

    Java

--- src/main/java/com/pcschool/ocp/d01/Account.java
+++ src/main/java/com/pcschool/ocp/d01/Account.java
@@ -1,6 +1,24 @@
 package com.pcschool.ocp.d01;
 
-public class Account {
+public class Account {    
+    private int balance;
+    
+    public void save(int money){
+        if(money>0){
+            balance=balance+money;       
+        }
+       
+    }
+    public void withdraw(int money){
+        if(money>0 && balance>=money){
+            balance=balance-money;       
+        }
+    
+    }
+    
+    public int getBalance(){
+        return balance;
+    }
     
     
 }
