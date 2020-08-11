34e5045ed5aa9028b3ffe70dbb05349e160b077c
MB-study <MB-study@192.168.1.88>
2020-08-11 10:28:42 +0800

    Java

--- src/main/java/com/pcschool/ocp/d01/Cat.java
+++ src/main/java/com/pcschool/ocp/d01/Cat.java
@@ -1,5 +1,9 @@
 package com.pcschool.ocp.d01;
 
 public class Cat {
-    
+    public static String type;
+    public int age;
+    public int price;
+    public int amount;
+    public String name;
 }
